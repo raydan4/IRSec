fd7175eb2ea5742ba05e01f6f757a35e4e58bcd9
raydan4 <dszafran@gmail.com>
2019-04-20 04:02:17 -0400

    Added apache hardening script

---
+++ ah.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+c=/etc/apache2/apache2.conf
+
+echo "ServerTokens Prod" >> $c
+echo "ServerSignature Off" >> $c
+sed -i 's/Options FollowSymlinks/Options -Indexesi\nAllowOverride None/g' $c
+echo "FileETag None" >> $c
+echo "TraceEnable off" >> $c
+echo "Header edit Set-Cookie ^(.*)$ $1;HttpOnly;Secure" >> $c
+echo "Header always append X-Frame-Options SAMEORIGIN" >> $c
+echo "Header set  X-XXS-Protection \"1; mode=block\"" >> $c
+service Apache restart
+
